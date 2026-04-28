7a91437881d174bce55cc37547659c88a0ce9c94
AbuHuraraDone <huraraa540@gmail.com>
2026-04-28 19:36:15 +0500

    Login screen Implemented

--- App.tsx
+++ App.tsx
@@ -1,37 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import { Alert, StyleSheet, Text, TextInput, TouchableHighlight, View, TouchableOpacity } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
+export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleLogin = () => {
+    if(username === '' || password === '') {
+      Alert.alert('Error', 'Please enter both username and password.');
+      return;
+    }
+    Alert.alert('Login Info', `Username: ${username}\nPassword: ${password}`);
+    setUsername('');
+    setPassword('');
+  }
+  const handleForgetPassword = () => {
+    Alert.alert('Forget Password', 'Password recovery is not implemented yet.');
+  }
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+      <Text style={styles.loginText}> User Login </Text>
+      <TextInput 
+          placeholder="Username"
+          style={styles.input} 
+          value={username}
+          onChangeText={setUsername}
       />
+      <TextInput 
+          placeholder="Password"
+          style={styles.input} 
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.forgetPassword} onPress={handleForgetPassword}>
+        <Text style={styles.forgetPasswordText}>Forget Password?</Text>
+      </TouchableOpacity>
+      <TouchableHighlight style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -39,7 +45,46 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-});
-
-export default App;
+  loginText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input:{
+    width: '80%',
+    height: 50,
+    borderColor: 'gray',
+    borderRadius: 5,
+    borderLeftWidth: 5,
+    borderWidth: 1,
+    marginTop: 20,
+    paddingHorizontal: 10,
+  },
+  forgetPassword: {
+    marginLeft: 40,
+    alignSelf: 'flex-start',
+    marginTop: 10,
+  },
+  forgetPasswordText: {
+    color: 'blue',
+  },
+  button: {
+    marginTop: 20,
+    width: '80%',
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '700',
+  },
+});   
